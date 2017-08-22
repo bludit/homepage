6a0ea91c92d1df40b4372cfcf66fc2e462774bc1
Diego Najar <dignajar@gmail.com>
2017-08-22 21:10:36 +0200

    facebook Cards

--- config.php
+++ config.php
@@ -13,7 +13,8 @@ $topbar = array(
 	'themes'=>'https://themes.bludit.com',
 	'plugins'=>'https://plugins.bludit.com',
 	'donations'=>'https://www.bludit.com#donations',
-	'pro'=>'https://pro.bludit.com'
+	'pro'=>'https://pro.bludit.com',
+	'website'=>DOMAIN
 );
 
 // Language
@@ -52,7 +53,8 @@ if ($defaultLanguage == "es") {
 		'themes'=>'https://themes.bludit.com/es/',
 		'plugins'=>'https://plugins.bludit.com/es/',
 		'donations'=>'https://www.bludit.com/es/#donations',
-		'pro'=>'https://pro.bludit.com/es/'
+		'pro'=>'https://pro.bludit.com/es/',
+		'website'=>DOMAIN.'/es/'
 	);
 } elseif ($defaultLanguage == "de") {
 	$defaultLocale = 'de_DE';
@@ -63,7 +65,8 @@ if ($defaultLanguage == "es") {
 		'themes'=>'https://themes.bludit.com/de/',
 		'plugins'=>'https://plugins.bludit.com/de/',
 		'donations'=>'https://www.bludit.com/de/#donations',
-		'pro'=>'https://pro.bludit.com/de/'
+		'pro'=>'https://pro.bludit.com/de/',
+		'website'=>DOMAIN.'/de/'
 	);
 }
 

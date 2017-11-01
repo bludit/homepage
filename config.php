ae4af46b6a6b548778a7c0541af8359535f8b1bd
Diego Najar <dignajar@gmail.com>
2017-11-01 11:28:09 +0100

    fix on config

--- config.php
+++ config.php
@@ -50,7 +50,7 @@ if ($defaultLanguage == "es") {
 	$topbar = array(
 		'download'=>'https://www.bludit.com/es/#download',
 		'demo'=>'https://www.bludit.com/es/#demo',
-		'docs'=>'https://docs.bludit.com/es/',
+		'docs'=>'https://docs.bludit.com',
 		'themes'=>'https://themes.bludit.com/es/',
 		'plugins'=>'https://plugins.bludit.com/es/',
 		'donations'=>'https://pro.bludit.com/es/#donate',
@@ -74,7 +74,7 @@ if ($defaultLanguage == "es") {
 	$topbar = array(
 		'download'=>'https://www.bludit.com/pl/#download',
 		'demo'=>'https://www.bludit.com/pl/#demo',
-		'docs'=>'https://docs.bludit.com/pl/',
+		'docs'=>'https://docs.bludit.com',
 		'themes'=>'https://themes.bludit.com/pl/',
 		'plugins'=>'https://plugins.bludit.com/pl/',
 		'donations'=>'https://pro.bludit.com/pl/#donate',

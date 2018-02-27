4f6b826d248d32134f71264a313a35b798fa8da9
Diego Najar <dignajar@gmail.com>
2018-02-27 14:03:52 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <!DOCTYPE HTML>
 <html>
 <head>
-	<meta charset="utf-8">
+	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<meta name="author" content="Diego Najar">
 	<meta name="description" content="<?php l('head description') ?>">

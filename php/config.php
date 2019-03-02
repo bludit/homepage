5d795ddbb140baa7463aafaa24b38ddca9b88f75
Diego Najar <dignajar@gmail.com>
2019-03-02 17:53:19 +0100

    change h3 for h2

--- php/config.php
+++ php/config.php
@@ -2,7 +2,7 @@
 define('DS', DIRECTORY_SEPARATOR);
 define('CHARSET', 'UTF-8');
 define('DOMAIN', 'https://www.bludit.com');
-define('VERSION', '3.6');
+define('VERSION', '3.8');
 
 define('DEFAULT_TWITTER_CARD', 'https://df6m0u2ovo2fu.cloudfront.net/images/bludit-twitter-cards.png');
 define('DEFAULT_FACEBOOK_CARD', 'https://df6m0u2ovo2fu.cloudfront.net/images/bludit-facebook-cards.png');
